de1bbcaa3133e127b049c79bf3bdcce4d93d4adf
Tanya Ramirez <tcramire@gmail.com>
2017-05-08 13:53:37 -0300

    Update archivo.js

--- assets/js/archivo.js
+++ assets/js/archivo.js
@@ -20,13 +20,3 @@ alert("Complete todos los campos")
 }
 }
 
-var peso= prompt("indique su peso");
-var altura=prompt("indique su altura");
-
-function calcularPeso(altura, peso){
-
-peso/Math.pow(altura,2);
-
-}
-
-calcularPeso(70, 1.70);
